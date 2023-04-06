5b46280f23b87e0addcb521150bad4d836ed8153
Debashish Sarkar <dsarkar@lutron.com>
2023-04-06 15:22:25 +0530

    Read Nuget Package Names from CSV

---
+++ NugetInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace nugetscan
+{
+    internal class NugetInfo
+    {
+        /// <summary>
+        /// Nuget Package ID (Newtonsoft.Json)
+        /// </summary>
+        public string? PackageID { get; set; }
+        /// <summary>
+        /// current nuget package version being used (6.5.0)
+        /// </summary>
+        public string? CurrentVersion { get; set; }
+        /// <summary>
+        /// latest nuget package version available with sources
+        /// </summary>
+        public string? LatestVersion { get; set; }
+
+    }
+}
---
+++ NugetInfoGatherer.cs
@@ -0,0 +1,39 @@
+﻿using CsvHelper;
+using System.Globalization;
+
+namespace nugetscan
+{
+    internal class NugetInfoGatherer
+    {
+        public void GetLatestNugetInfo()
+        {
+            var packages = ReadNugetList();
+            GetLatestNugetInfo(packages);
+            SaveResults(packages);
+        }
+
+        private List<NugetInfo> GetLatestNugetInfo(List<NugetInfo> packages)
+        {
+            return packages;
+        }
+        private List<NugetInfo> ReadNugetList()
+        {
+            List<NugetInfo> packages;
+            using (var reader = new StreamReader("input.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                packages = new List<NugetInfo>();
+                while (csv.Read())
+                {
+                    packages.Add(new NugetInfo() { PackageID = csv[0] });
+                }
+            }
+            return packages;
+        }
+
+        private void SaveResults(List<NugetInfo> packages)
+        {
+
+        }
+    }
+}
